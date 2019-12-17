d705c0b28618c6e7f343c5ff45cebf1e70513d95
AjitSingh12 <46068162+AjitSingh12@users.noreply.github.com>
2019-12-17 12:05:41 +0530

    Update gitrun.sh

--- gitrun.sh
+++ gitrun.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd ~/pcc/
-sudo rm -rf EliteCSM
-git clone git@gitlab.sterlitetech-software.com:EliteCSM/Development/EliteCSM.git
+#sudo rm -rf EliteCSM
+#git clone git@gitlab.sterlitetech-software.com:EliteCSM/Development/EliteCSM.git
+cd EliteCSM
+git checkout NETVERTEX-5728
+git pull origin NETVERTEX-5728
 sed -i s/pcc-docker-repo:5043[/]//g /home/sterlite/pcc/EliteCSM/Applications/nvsmx/docker/Dockerfile
 cd EliteCSM/Applications/netvertex/docker
 chmod 777 build-nvsmx-image.sh
